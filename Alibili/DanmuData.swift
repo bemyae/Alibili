862afbbd433c50fab197b834962b59db8041b27b
sauvignon1989 <zxndior@gmail.com>
2019-07-20 22:10:18 +0900

    change filename

---
+++ Alibili/DanmuData.swift
@@ -0,0 +1,9 @@
+//
+//  DanmuData.swift
+//  Alibili
+//
+//  Created by Xiaonan Zhang on 2019/07/20.
+//  Copyright © 2019 Xiaonan Zhang. All rights reserved.
+//
+
+import Foundation
--- Alibili/VideoPlayViewController.swift
+++ Alibili/VideoPlayViewController.swift
@@ -18,7 +18,7 @@ class VideoPlayerViewController: UIViewController, BarrageRendererDelegate {
     @IBOutlet var playerView: UIView!
     
     private let cookieManager:CookieManager = CookieManager()
-    var videoJson:SubscriptionsCellDateItem!
+    var videoJson:SubscriptionsCellDataItem!
     let player:VLCMediaPlayer = VLCMediaPlayer()
     var _index:Int = 0
     
@@ -27,9 +27,7 @@ class VideoPlayerViewController: UIViewController, BarrageRendererDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-         configDanMu()
-       
-//        loadItemData(avId: videoJson.id, pageNum: 1)
+        loadVideoData(avId: videoJson.id, pageNum: 1)
     }
     func walkTextSpriteDescriptorWithDirection(direction:UInt) -> BarrageDescriptor{
         let descriptor:BarrageDescriptor = BarrageDescriptor()
@@ -69,32 +67,17 @@ class VideoPlayerViewController: UIViewController, BarrageRendererDelegate {
         
     }
     
-    func startMockingBarrageMessage(){
-       
+    override func viewWillDisappear(_ animated: Bool) {
+        barrageRenderer.stop()
     }
     
-//    @objc func autoSendBarrage() {
-//        print("test")
-//        barrageRenderer.start()
-//        let danmuku:BarrageDescriptor = BarrageDescriptor.init()
-//        danmuku.spriteName = NSStringFromClass(BarrageWalkTextSprite.self)
-//        danmuku.params["text"] = "HHHHHHH";
-//        danmuku.params["textColor"] = UIColor.red
-//        danmuku.params["direction"] = BarrageWalkDirection.R2L
-//        danmuku.params["side"] = BarrageWalkSide.default
-//        barrageRenderer.receive(danmuku)
-//    }
     
-    func loadItemData(avId:String,pageNum:Int) -> Void {
-//        let headers: HTTPHeaders = [
-//            "Set-Cookie":cookieManager.getUserCookie(forKey: "User-Cookie")!,
-//            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3260.2 Safari/537.36"
-//        ]
+    func loadVideoData(avId:String,pageNum:Int) -> Void {
         AF.request("https://api.bilibili.com/x/web-interface/view?aid=\(avId)").responseJSON { response in
             switch(response.result) {
             case .success(let data):
                 let json = JSON(data)
-                //                print(json)
+                print(json["data"]["cid"].stringValue)
                 AF.request("https://api.bilibili.com/x/player/playurl?avid=\(avId)&cid=\(json["data"]["cid"].stringValue)&qn=80&type=&fnver=0&fnval=16&otype=json")
                     .responseJSON  { response in
                         switch(response.result) {
@@ -115,51 +98,13 @@ class VideoPlayerViewController: UIViewController, BarrageRendererDelegate {
                             self.player.addPlaybackSlave(URL(string: audio)!, type: VLCMediaPlaybackSlaveType.audio, enforce: true)
                             self.player.drawable = self.playerView
                             self.player.play()
+                            self.configDanMu()
                             return
-                            //                            do {
-                            //                                try AVAudioSession.sharedInstance().setCategory(AVAudioSession.Category.playback)
-                            //                                try AVAudioSession.sharedInstance().setActive(true)
-                            //
-                            //                                let player = try AVAudioPlayerNode()
-                            //
-                            //                                player.play()
-                            //
-                            //                            } catch let error {
-                            //                                print(error.localizedDescription)
-                            //                            }
-                            
-                            
-                            //                            let headersForVideo: HTTPHeaders = [
-                            //                                "Set-Cookie":self.cookieManager.getUserCookie(forKey: "User-Cookie")!,
-                            //                                "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3260.2 Safari/537.36",
-                            //                                "Referer":"https://www.bilibili.com/video/av\(avId)",
-                            //                                "Origin":"https://www.bilibili.com",
-                            //                                "Accept":"*/*",
-                            //                                "Accept-Encoding":"gzip, deflate, sdch, br",
-                            //                                "Accept-Language":"zh-CN,zh;q=0.8"
-                            //                            ]
-                            //
-                            //                            AF.request(dataarray[0]["base_url"].stringValue, headers: headersForVideo).response { response in
-                            //                                print(response.debugDescription)
-                            //                            }
-                            
-//                            let player = AVPlayer(url: URL(string: video)!)
-//                            let playerViewController = AVPlayerViewController()
-//                            playerViewController.player = player
-//
-//                            self.present(playerViewController, animated: true) {
-//                                player.play()
-//                            }
-                            
-                            
-                            
                         case .failure(let error):
                             print(error)
                             break
                         }
-                        
                 }
-                
             case .failure(let error):
                 print(error)
                 break
